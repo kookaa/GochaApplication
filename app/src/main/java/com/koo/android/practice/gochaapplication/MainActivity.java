311b0925575f683d8101731b90eb564d810c8d54
kookaa <koookaaati@gmail.com>
2015-08-05 02:13:05 +0900

    use TextView

--- app/src/main/java/com/koo/android/practice/gochaapplication/MainActivity.java
+++ app/src/main/java/com/koo/android/practice/gochaapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.text);
+        textView.setText("Hello New World!");
     }
 
     @Override
